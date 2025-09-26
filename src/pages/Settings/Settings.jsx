0fbc74c19962892adc2b5794e2a3f10f22675a24
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-09-25 22:14:11 -0400

    Updated Settings styling

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react"
+import styles from "./Settings.module.scss"
 
 export default function Settings({ theme, setTheme }) {
     return (
-        <>
+        <div className={styles.settings}>
             <h1>Settings</h1>
-            <p>Choose a theme:</p>
-            <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-                <option value="dark">Dark</option>
-                <option value="light">Light</option>
-                <option value="forest">Forest</option>
-                <option value="ocean">Ocean</option>
-                <option value="pastel">Pastel</option>
-                <option value="cyberpunk">Cyberpunk</option>
-            </select>
-        </>
+            <div className={styles.settingsEntry}>
+                <label>Choose a theme:</label>
+                <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+                    <option value="dark">Dark</option>
+                    <option value="light">Light</option>
+                    <option value="forest">Forest</option>
+                    <option value="ocean">Ocean</option>
+                    <option value="pastel">Pastel</option>
+                    <option value="cyberpunk">Cyberpunk</option>
+                </select>
+            </div>
+        </div>
     )
 }
