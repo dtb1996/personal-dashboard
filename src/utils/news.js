df4294ebc77fb169fc0dd4c708457de78a3cda2c
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-09-29 23:04:39 -0400

    Include AbortController signal handling in fetchNews()

--- src/utils/news.js
+++ src/utils/news.js
@@ -1,7 +1,9 @@
-export async function fetchNews() {
+export async function fetchNews({ signal } = {}) {
     const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
-    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`, {
+        signal,
+    })
 
     if (!res.ok) {
         throw new Error(`Error ${res.status}`)
