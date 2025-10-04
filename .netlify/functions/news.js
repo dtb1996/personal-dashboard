3717e5a8b820193abfa54647c8b66aa44d08a8c1
Dillon Bellefeuille <dtb1996@yahoo.com>
2025-10-04 17:35:56 -0400

    Updated url

--- .netlify/functions/news.js
+++ .netlify/functions/news.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch"
 
 export async function handler(_event) {
     const API_KEY = process.env.NEWS_API_KEY
-    const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=${API_KEY}`
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 
     try {
         const res = await fetch(url)
